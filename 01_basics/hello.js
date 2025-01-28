4a70a6723f4cf60d8bc79d3ec8689c2df7c944f0
Arghyadeep Banerjee <59882956+Arghyadeep858@users.noreply.github.com>
2025-01-28 07:32:25 +0000

    js datatyes and object

--- 01_basics/hello.js
+++ 01_basics/hello.js
@@ -1,1 +1,29 @@
-console.log("hello");
+var name = "arghya";
+let city = "kolkata";
+const accountId = "1234";
+
+var name = "asd";
+ city = "kolasdkata";
+
+console.table([name, city, accountId]);
+
+
+/*data types*/
+
+//number
+//bigInt
+//boolean
+//null
+//undefined
+//String
+//symbol
+
+
+/*Object*/
+
+console.log(typeof "Arghya"); //String
+console.log(typeof 4); // Number
+console.log(typeof null); //Object
+console.log(typeof undefined);//Undefined 
+
+
